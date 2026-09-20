6605c47e0ec79d04b7572a901be9a50f71f58ad9
houyuji <877286291@qq.com>
2026-09-20 09:17:19 +0800

    chore: 移除 lightbox（已迁至 aurora-app-lab/lightbox）

--- lightbox/src/app/detail/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import DetailPageClient from "@/components/DetailPageClient";
-
-interface PageProps {
-  params: { id: string };
-}
-
-export default function DetailPage({ params }: PageProps) {
-  return <DetailPageClient id={params.id} />;
-}
